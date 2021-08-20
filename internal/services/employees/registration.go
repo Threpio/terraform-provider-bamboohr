01a1ba04f17c7ea1bbd938ad4c7125ee459c1ff3
Threpio <Theomorris@hotmail.co.uk>
2021-08-20 15:53:58 +0200

    missed Supported Resource requirement for services

--- internal/services/employees/registration.go
+++ internal/services/employees/registration.go
@@ -26,9 +26,7 @@ func (r Registration) SupportedDataSources() map[string]*schema.Resource {
 	}
 }
 
-//// SupportedResources returns the supported Resources supported by this Service
-//func (r Registration) SupportedResources() map[string]*schema.Resource {
-//	return map[string]*schema.Resource{
-//		"azuread_user": userResource(),
-//	}
-//}
+// SupportedResources returns the supported Resources supported by this Service
+func (r Registration) SupportedResources() map[string]*schema.Resource {
+	return map[string]*schema.Resource{}
+}
